6fd6443cbdfb85d8255e6948497133335165b493
eyalhoc <eyalhoc@69389a9c-fa11-4abd-b875-5c99056e2f99>
2011-03-30 16:27:37 +0000

    fixed run script

--- run/run.sh
+++ run/run.sh
@@ -6,4 +6,4 @@ mkdir out
 
 ../../../robust ../src/base/ic.v -od out -I ../src/gen -list iclist.txt -listpath -header
 
-echo Completed RobustVerilog fabric run - results in examples/fabric/run/out/
+echo Completed RobustVerilog fabric run - results in run/out/
