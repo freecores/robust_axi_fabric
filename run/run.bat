18b0e39fae4a02b7f72cd1d4558009aad06e0a73
eyalhoc <eyalhoc@69389a9c-fa11-4abd-b875-5c99056e2f99>
2011-04-11 15:44:22 +0000

    added dos batch file for windows

---
+++ run/run.bat
@@ -0,0 +1,6 @@
+
+echo off
+
+..\..\..\robust.exe ../src/base/ic.v -od out -I ../src/gen -list iclist.txt -listpath -header
+
+echo Completed RobustVerilog fabric run - results in run/out/
--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo Starting RobustVerilog fabric run
-rm -rf out
-mkdir out
-
 ../../../robust ../src/base/ic.v -od out -I ../src/gen -list iclist.txt -listpath -header
 
 echo Completed RobustVerilog fabric run - results in run/out/
